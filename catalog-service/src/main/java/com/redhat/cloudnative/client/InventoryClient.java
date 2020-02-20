e586c605c95e9158e6c2cfb8c13c5fce2f98ec4d
danieloh30 <doh@redhat.com>
2020-02-20 00:43:29 -0500

    update quarkus version

--- catalog-service/src/main/java/com/redhat/cloudnative/client/InventoryClient.java
+++ catalog-service/src/main/java/com/redhat/cloudnative/client/InventoryClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name="inventory",fallbackFactory = InventoryClient.InventoryClientFallbackFactory.class)
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/services/inventory/{itemId}", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory/{itemId}", consumes = {MediaType.APPLICATION_JSON_VALUE})
     String getInventoryStatus(@PathVariable("itemId") String itemId);
 
     //TODO: Add Fallback factory here
