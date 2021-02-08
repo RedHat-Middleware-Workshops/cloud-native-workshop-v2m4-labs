6ed19c087cbc2b8c698afde725c42461f42acc22
danieloh30 <doh@redhat.com>
2021-02-08 10:41:53 -0500

    Fix the duplicated code

--- cart-service/src/main/java/com/redhat/cloudnative/model/Promotion.java
+++ cart-service/src/main/java/com/redhat/cloudnative/model/Promotion.java
@@ -34,39 +34,3 @@ public class Promotion {
     }
 
 }
-package com.redhat.cloudnative.model;
-
-import org.infinispan.protostream.annotations.ProtoFactory;
-import org.infinispan.protostream.annotations.ProtoField;
-
-public class Promotion {
-
-    private String itemId;
-    private double percentOff = 0.0f;
-
-    @ProtoFactory
-    public Promotion(String itemId, double percentOff) {
-        super();
-        this.itemId = itemId;
-        this.percentOff = percentOff;
-    }
-
-    @ProtoField(number = 1)
-    public String getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(String itemId) {
-        this.itemId = itemId;
-    }
-
-    @ProtoField(number = 2, required = false, defaultValue = "0.0f")
-    public double getPercentOff() {
-        return percentOff;
-    }
-
-    public void setPercentOff(double percentOff) {
-        this.percentOff = percentOff;
-    }
-
-}
