ece2c0279b2a5f0187c1610b75d18a7b2c412f7a
danieloh30 <doh@redhat.com>
2021-12-10 22:16:50 -0500

    Update crw 2.13, rhbq 2.2, and spring boot 2.4.9

--- catalog-service/src/main/java/com/redhat/cloudnative/client/InventoryClient.java
+++ catalog-service/src/main/java/com/redhat/cloudnative/client/InventoryClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import feign.hystrix.FallbackFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
 
 @FeignClient(name="inventory", url = "${inventory.url}",fallbackFactory = InventoryClient.InventoryClientFallbackFactory.class)
 public interface InventoryClient {
