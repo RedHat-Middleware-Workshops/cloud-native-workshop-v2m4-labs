f4ce8aeac9230f5b266e9d7f17a5c6dab4e99c3c
danieloh30 <doh@redhat.com>
2021-12-15 10:09:08 -0500

    Update ocp4-workload-ccn-cuttingedge for RHBQ2.2 and CRW 2.13

--- cart-service/src/main/java/com/redhat/cloudnative/CartResource.java
+++ cart-service/src/main/java/com/redhat/cloudnative/CartResource.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 
 // import org.apache.kafka.clients.producer.KafkaProducer;
 // import org.apache.kafka.clients.producer.Producer;
@@ -40,35 +39,25 @@ public class CartResource {
 
     @POST
     @Path("{cartId}/{itemId}/{quantity}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ShoppingCart add(@PathParam("cartId") String cartId,
-                            @PathParam("itemId") String itemId,
-                            @PathParam("quantity") int quantity) throws Exception {
+    public ShoppingCart add(String cartId, String itemId, int quantity) throws Exception {
         return shoppingCartService.addItem(cartId, itemId, quantity);
     }
 
     @POST
     @Path("{cartId}/{tmpId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ShoppingCart set(@PathParam("cartId") String cartId,
-                            @PathParam("tmpId") String tmpId) throws Exception {
+    public ShoppingCart set(String cartId, String tmpId) throws Exception {
         return shoppingCartService.set(cartId, tmpId);
     }
 
     @DELETE
     @Path("{cartId}/{itemId}/{quantity}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public ShoppingCart delete(@PathParam("cartId") String cartId,
-                               @PathParam("itemId") String itemId,
-                               @PathParam("quantity") int quantity) throws Exception {
+    public ShoppingCart delete(String cartId, String itemId, int quantity) throws Exception {
         return shoppingCartService.deleteItem(cartId, itemId, quantity);
     }
 
     @POST
     @Path("/checkout/{cartId}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public ShoppingCart checkout(@PathParam("cartId") String cartId, Order order) {
+    public ShoppingCart checkout(String cartId, Order order) {
         // TODO ADD for KAFKA
         //sendOrder(order, cartId);
         return shoppingCartService.checkout(cartId);
