97b9920b89107a6d4e6b40ea2064f62db94f4a1a
Daniel Oh <doh@redhat.com>
2023-10-24 11:08:22 -0400

    Update reactive jackson

--- inventory-service/src/main/java/com/redhat/cloudnative/InventoryResource.java
+++ inventory-service/src/main/java/com/redhat/cloudnative/InventoryResource.java
@@ -7,13 +7,12 @@ import jakarta.json.Json;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
-
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/api/inventory")
 @Produces("application/json")
